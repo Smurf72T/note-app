317d8ba34c34d6956f54e11ffce42b02df80a0cb
Smurf <myskkk@yandex.ru>
2024-08-12 09:25:34 +0500

    create edit_note

--- note app.py
+++ note app.py
@@ -1,4 +1,7 @@
 """Функция для создания заметки в новом файле"""
+import os.path
+
+
 def build_note(note_text, note_name):
     f = open(note_name, 'w')
     f.write(note_text)
@@ -7,11 +10,20 @@ def build_note(note_text, note_name):
 """Функция, которая выводит заметку по запросу пользователя"""
 def create_note():
     note_name = input()
+    note_text = input()
     try:
-        f = open(note_name, 'r')
-        print(f)
+        build_note(note_text, note_name)
+        # f = open(note_name, 'r')
+        # print(f)
     except:
         print(f'Заметка {note_name} не найдена')
 
 """Функция, которая редактирует заметку"""
 def edit_note():
+    note_name = input()
+    if os.path.isfile(note_name):
+        f = open(note_name, 'r')
+        f.close()
+        print(f)
+    else:
+        print(f'Заметка {note_name} не найдена')
