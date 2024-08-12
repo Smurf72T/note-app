4c519fbb8306f1970fbaef6a6717f8c2862d2c0b
Smurf <myskkk@yandex.ru>
2024-08-12 10:46:49 +0500

    add main_note

--- note app.py
+++ note app.py
@@ -1,13 +1,18 @@
 import os.path
 
 """Функция для создания заметки в новом файле"""
+
+
 def build_note(note_text, note_name):
     f = open(f'{note_name}.txt', 'w', encoding="utf-8")
     f.write(note_text)
     f.close()
     print(f'Заметка {note_name} создана')
 
+
 """Функция, которая запрашивает название и текст заметки у пользователя, после чего создает заметку"""
+
+
 def create_note():
     note_name = input("Введите название заметки: ")
     note_text = input("Введите текст заметки: ")
@@ -16,21 +21,27 @@ def create_note():
     # except:
     #     print(f'Заметка {note_name} не найдена')
 
+
 """Функция, которая выводит заметку по запросу пользователя"""
+
+
 def read_note():
     note_name = input()
     if os.path.isfile(f'{note_name}.txt'):
-        f = open(f'{note_name}.txt', 'r')
+        f = open(f'{note_name}.txt', 'r', encoding="utf-8")
         f.close()
         print(f)
     else:
         print(f'Заметка {note_name} не найдена')
 
+
 """Функция которая редактирует заметку"""
+
+
 def edit_note():
     note_name = input("Введите название заметки: ")
     if os.path.isfile(f'{note_name}.txt'):
-        f = open(f'{note_name}.txt', '+')
+        f = open(f'{note_name}.txt', '+', encoding="utf-8")
         print(f)
         note_text = input("Введите текст заметки: ")
         f.write(note_text)
@@ -38,10 +49,32 @@ def edit_note():
     else:
         print(f'Заметка {note_name} не найдена')
 
+
 """Функция, которая удаляет заметку"""
+
+
 def delete_note():
     note_name = input("Введите название заметки: ")
     if os.path.isfile(f'{note_name}.txt'):
         os.remove(f'{note_name}.txt')
     else:
         print(f'Заметка {note_name} не найдена')
+
+"""Функция, которая управляет работой приложения"""
+def main_note():
+    print(f"""Функции программы заметки
+          1. Создание заметки
+          2. Редактирование заметки
+          3. Удаление заметки
+          4. Просмотр заметки""")
+
+    num_fun = input('Введите номер желаемого пункта: ')
+    for i in num_fun:
+        if i.isdigit() is not True:
+            raise ValueError("Некорректный ввод. Выберите один из пунктов")
+    if num_fun > 4:
+        raise ValueError("Введите значение от 1 до 4")
+        main_note()
+    else:
+        note_function = ['Hi', create_note, read_note, edit_note, delete_note]
+        return note_function.index(num_fun)
